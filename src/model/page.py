465df3bed9f5c5d6bb48b8f73c578760a66d2e95
matryus <matheusuzl18@gmail.com>
2017-04-22 16:45:08 -0300

    page_and_user

--- src/model/page.py
+++ src/model/page.py
@@ -1,11 +1,44 @@
-class Page(object):
-    '''
-    classdocs
-    '''
 
 
-    def __init__(self):
-        '''
-        Constructor
-        '''
-        
+class Pages(obejct):
+
+	def _init_(self,page_name, page_id, category): 
+		self.__page_name = page_name
+		self.__page_id = page_id
+		self.__category = category
+	
+
+	def set_id(self,page_id):
+		self.__page_id = page_id
+
+	def get_id(self):
+		return self.__page_id
+
+	def set_name(self,name):
+		self.__page_name = page_name
+
+	def get_name(self):
+		return self.__page_name
+
+	def set_category(self,category):
+		self.__category = category
+
+	def get_category(self):
+		return self.__category
+
+
+	def page_as_dict():
+		 return {
+            'page_id' : self.__page_id,
+            'page_name' : self.__page_name,
+            'category' : self.__category,
+            
+        }
+    page_id = property(get_id, set_id, None, None)
+    page_name = property(get_name, set_name, None, None)
+    category = property(get_category, set_category, None, None)
+    
+
+
+
+    
--- src/model/user.py
+++ src/model/user.py
@@ -1,11 +1,70 @@
-class User(object):
-    '''
-    classdocs
-    '''
 
 
-    def __init__(self):
-        '''
-        Constructor
-        '''
-        
+class User(obejct):
+
+	def _init_(self,facebook_id, initials, gender, birthday, location,pages): 
+		self.__facebook_id = facebook_id
+		self.__initials = initials
+		self.__gender = gender 
+		self.__birthday = birthday
+		self.__location = location
+		self.__pages = pages
+
+	
+
+	def set_id(self,facebook_id):
+		self.__facebook_id = facebook_id
+
+	def get_id(self):
+		return self.__facebook_id
+
+	def set_initials(self,initials):
+		self.__initials_id = initials
+
+	def get_initials(self):
+		return self.__initials
+
+	def set_gender(self,gender):
+		self.__gender = gender
+
+	def get_gender(self):
+		return self.__gender
+
+	def set_birthday(self,birthday):
+		self.__birthday = birthday
+
+	def get_birthday(self):
+		return self.__birthday
+
+	def set_location(self,location):
+		self.__location = location
+
+	def get_location(self):
+		return self.__location
+
+	def set_pages(self, pages):
+		self.__pages = pages
+
+	def get_pages(self):
+		return self.__pages
+
+	def user_as_dict():
+		 return {
+            'facebook_id' : self.__facebook_id,
+            'initials' : self.__initials,
+            'gender' : self.__gender,
+            'birthday' : self.__birthday,
+            'location' : self.__location,
+            'pages' : self.__pages
+        }
+    facebook_id = property(get_id, set_id, None, None)
+    initials = property(get_initials, set_initials, None, None)
+    gender = property(get_gender, set_gender, None, None)
+    birthday = property(get_birthday, set_birthday, None, None)
+    location = property(get_location, set_location, None, None)
+    pages = property(get_pages, set_pages, None, None)
+
+
+
+
+    
