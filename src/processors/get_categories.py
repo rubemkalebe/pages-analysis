04c7d64324771eef6a365db956b987b4bfe3bc2c
Rubem Kalebe <rubemkalebe@hotmail.com>
2017-04-23 13:21:30 -0300

    Returns a dict instead of a string

--- src/processors/get_categories.py
+++ src/processors/get_categories.py
@@ -1,4 +1,4 @@
-from helpers import categories
+from ..helpers import categories
 
 class GetCategories(object):
     '''
@@ -8,9 +8,13 @@ class GetCategories(object):
 
     @staticmethod
     def get(udao, location = 'Natal'):
-        cs = ''
+        dic = {}
         for u in udao.fetchall():
             if location in u.location:
                 for p in u.pages:
-                    cs += (categories.categories[p.category] + ' ')
-        return cs
+                    if categories.categories[p.category] in dic:
+                        dic[categories.categories[p.category]] += 1
+                    else:
+                        dic[categories.categories[p.category]] = 1
+        print(dic)
+        return dic
