5223a3fdd2d1e32ad3bffc402b51e32c98435912
matryus <matheusuzl18@gmail.com>
2017-04-23 16:10:55 -0300

    class String

--- src/processors/get_categories.py
+++ src/processors/get_categories.py
@@ -8,13 +8,9 @@ class GetCategories(object):
 
     @staticmethod
     def get(udao, location = 'Natal'):
-        dic = {}
+        cs = ''
         for u in udao.fetchall():
             if location in u.location:
                 for p in u.pages:
-                    if categories.categories[p.category] in dic:
-                        dic[categories.categories[p.category]] += 1
-                    else:
-                        dic[categories.categories[p.category]] = 1
-        print(dic)
-        return dic
+                    cs += (categories.categories[p.category] + ' ')
+        return cs
