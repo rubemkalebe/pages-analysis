bc17645a9812cb35fd37e63a96552dc461df2bdc
Rubem Kalebe <rubemkalebe@hotmail.com>
2017-04-23 23:35:06 -0300

    Handling as dict

--- src/processors/get_categories.py
+++ src/processors/get_categories.py
@@ -7,10 +7,23 @@ class GetCategories(object):
 
 
     @staticmethod
-    def get(udao, location = 'Natal'):
+    def get_as_string(udao, location = 'Natal'):
         cs = ''
         for u in udao.fetchall():
             if location in u.location:
                 for p in u.pages:
-                    cs += (categories.categories[p.category] + ' ')
+                    cs += (categories.categories_no_space[p.category] + ' ')
         return cs
+
+
+    @staticmethod
+    def get_as_dict(udao, location = 'Natal'):
+        dic = {}
+        for u in udao.fetchall():
+            if location in u.location:
+                for p in u.pages:
+                    if categories.categories_with_spaces[p.category] in dic:
+                        dic[categories.categories_with_spaces[p.category]] += 1
+                    else:
+                        dic[categories.categories_with_spaces[p.category]] = 1
+        return dic
